263b02e0ce6f33573cc723f317d1228b5c6cdefb
Guilherme <guiciani@gmail.com>
2021-11-18 14:10:39 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+# Import all necessary libraries for loading our data
+# Define and intialize the neural network A and B
+# Save model A
+# Load into model B
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class NetA(nn.Module):
+    def __init__(self):
+        super(NetA, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+netA = NetA()
+
+class NetB(nn.Module):
+    def __init__(self):
+        super(NetB, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+netB = NetB()
+
+# Specify a path to save to
+PATH = "model.pt"
+
+torch.save(netA.state_dict(), PATH)
+
+netB.load_state_dict(torch.load(PATH), strict=False)
